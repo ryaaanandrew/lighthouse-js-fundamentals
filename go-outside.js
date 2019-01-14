2f8dd96df2dac2d0c24d4bc3d420b622aba98498
ryan andrew sibat <ryanandrew.sibat@gmail.com>
2019-01-14 06:11:24 +0000

    Added conditionals excerises

---
+++ go-outside.js
@@ -0,0 +1,27 @@
+var temperature = 12;
+var isCitizen = true;
+var age = 30;
+var raining = false;
+
+
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
